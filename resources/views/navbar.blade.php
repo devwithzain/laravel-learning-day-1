1757fd43d4b084559d98a693e8d500482119862c
devwithzain <ZainSoftwear11@gmail.com>
2024-10-29 03:18:52 +0500

    modified

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Navbar</title>
+</head>
+
+<body>
+   <div>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/work">Work</a>
+      <a href="/process">Process</a>
+      <a href="/services">Services</a>
+      <a href="/contact">Contact</a>
+   </div>
+</body>
+
+</html>
--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -4,11 +4,26 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   @vite('resources/css/app.css')
    <title>Work Page</title>
 </head>
 
 <body>
-   <h1>Work Page</h1>
+   <div class="flex items-center justify-center w-full h-screen">
+      <div class="absolute top-0 left-0 w-full bg-gray-200 px-10 py-3">
+         <div class="flex items-center gap-3">
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/">Home</a>
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/about">About</a>
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/work">Work</a>
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/process">Process</a>
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/services">Services</a>
+            <a class="text-lg text-black uppercase font-semibold font-sans" href="/contact">Contact</a>
+         </div>
+      </div>
+      <h1 class="text-black text-2xl font-semibold font-sans uppercase">
+         Work Page
+      </h1>
+   </div>
 </body>
 
 </html>
